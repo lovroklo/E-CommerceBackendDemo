a89b483084f945920189d85d95eac210b6e273be
lovroklo <lovro.klobucaric@gmail.com>
2022-09-17 23:05:08 +0200

    Added variation with functionality to find by id, categoryId and saving (I still need to add update and delete crud operations)

--- src/main/java/hr/klobucaric/webshop/variation/VariationRepository.java
+++ src/main/java/hr/klobucaric/webshop/variation/VariationRepository.java
@@ -1,8 +1,21 @@
 package hr.klobucaric.webshop.variation;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Set;
 
 public interface VariationRepository extends JpaRepository<Variation, Long> {
 
+    @Query("""
+      select new hr.klobucaric.webshop.variation.VariationDto(v.id, v.name) from Variation v
+      where v.category.id = :categoryId
+    """)
+    Set<VariationDto> findVariationDtoSetByCategoryId(Long categoryId);
 
+    @Query("""
+      select new hr.klobucaric.webshop.variation.VariationDto(v.id, v.name) from Variation v
+      where v.id = :id
+    """)
+    VariationDto findDtoById(Long id);
 }
---
+++ src/main/java/hr/klobucaric/webshop/variation/VariationService.java
@@ -0,0 +1,12 @@
+package hr.klobucaric.webshop.variation;
+
+
+import java.util.Set;
+
+public interface VariationService {
+
+    Set<VariationDto> findVariationsByCategoryId(Long categoryId);
+    VariationDto findVariationById(Long id);
+    VariationDto save(final VariationCommand variationCommand);
+
+}
