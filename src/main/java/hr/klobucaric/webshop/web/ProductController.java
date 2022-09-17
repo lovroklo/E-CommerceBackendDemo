9c9752bc789cb655c1b2533a26477b9d65307fd1
lovroklo <lovro.klobucaric@gmail.com>
2022-09-17 23:11:50 +0200

    Added new functionality to find all products based on provided category id, it will also provide all products that are in subcategories of that category

--- src/main/java/hr/klobucaric/webshop/web/ProductController.java
+++ src/main/java/hr/klobucaric/webshop/web/ProductController.java
@@ -6,34 +6,31 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/product")
-@CrossOrigin(origins = "http://localhost:4200")
 public class ProductController {
 
     private final ProductService productService;
 
     @GetMapping
     public ResponseEntity<Map<String,Object>> getAllProducts(@RequestParam(name = "p", defaultValue = "0")  Integer pageNumber,
-                                                           @RequestParam(name = "numberOfProducts", defaultValue = "12")  Integer numberOfProducts) {
-        Page<ProductDto> productPage = productService.findAllByPagination(pageNumber, numberOfProducts);
+                                                             @RequestParam(name = "n", defaultValue = "12")  Integer numberOfProducts,
+                                                             @RequestParam(name = "ctg") Long categoryId) {
+        Page<ProductDto> productPage;
 
-        Map<String, Object> response = new HashMap<>();
-        response.put("products", productPage.getContent());
-        response.put("currentPage", productPage.getNumber());
-        response.put("totalItems", productPage.getTotalElements());
-        response.put("totalPages", productPage.getTotalPages());
+        if(categoryId!=null){
+            productPage =  productService.findByCategoryId(categoryId, pageNumber, numberOfProducts);
+        } else{
+            productPage = productService.findAllByPagination(pageNumber,numberOfProducts);
+        }
 
+        Map<String, Object> response = getStringObjectMap(productPage);
         return new ResponseEntity<>(response,HttpStatus.OK);
     }
 
@@ -42,11 +39,6 @@ public class ProductController {
         return new ResponseEntity<>(productService.findById(id),HttpStatus.OK);
     }
 
-
-    @GetMapping("/category/{id}")
-    public ResponseEntity<List<ProductDto>> getProductByCategoryId(@PathVariable final Long id) {
-        return new ResponseEntity<>(productService.findByCategoryId(id), HttpStatus.OK);
-    }
 
     @PostMapping
     public ResponseEntity<ProductDto> save(@Valid @RequestBody final ProductCommand productCommand){
@@ -60,12 +52,19 @@ public class ProductController {
 
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/{id}")
-    public void deleteByCode(@PathVariable Long id){
+    public void deleteById(@PathVariable Long id){
         productService.deleteById(id);
     }
 
 
-
+    private Map<String, Object> getStringObjectMap(Page<ProductDto> productPage) {
+        Map<String, Object> response = new HashMap<>();
+        response.put("products", productPage.getContent());
+        response.put("currentPage", productPage.getNumber());
+        response.put("totalItems", productPage.getTotalElements());
+        response.put("totalPages", productPage.getTotalPages());
+        return response;
+    }
 
 
 
